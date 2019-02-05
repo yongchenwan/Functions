67a74d3fbcfc33b2949cbf5949a3527f6caefded
yongchenwan <37253552+yongchenwan@users.noreply.github.com>
2019-02-05 02:02:07 -0500

    Update print_ckpt.py

--- print_ckpt/print_ckpt.py
+++ print_ckpt/print_ckpt.py
@@ -2,13 +2,4 @@ import tensorflow as tf
 # from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
 from inspect_checkpoint import print_tensors_in_checkpoint_file
 savedir = "/Users/yongchenwan/Downloads/log_big"
-# print_tensors_in_checkpoint_file(savedir + "linermodel.cpkt", None, True)
-# W = tf.Variable(1.0, name="weight")
-# b = tf.Variable(2.0, name="bias")
-# # 放到一个字典里:
-# saver = tf.train.Saver({'weight': b, 'bias': W})
-# with tf.Session() as sess:
-#     tf.global_variables_initializer().run()
-#     saver.save(sess, savedir+"linermodel.cpkt")
-# print_tensors_in_checkpoint_file(savedir+"linermodel.cpkt", None, True)
 print_tensors_in_checkpoint_file(tf.train.latest_checkpoint(savedir), None, True)
